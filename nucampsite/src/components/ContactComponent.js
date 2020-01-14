d5fe5e21f1cef09a0006f7c8d7439491df7c7f6a
cjiles <57699863+cjiles@users.noreply.github.com>
2020-01-13 22:49:08 -0600

    Uncontrolled Form

--- nucampsite/src/components/ContactComponent.js
+++ nucampsite/src/components/ContactComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Label, Input, Col, FormFeedback} from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, 
+    Label, Input, Col, FormFeedback} from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { render } from '@testing-library/react';
+
 
 class Contact extends Component {
 
