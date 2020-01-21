eec79351eb72b15b943d17614262f1af0c2c13d1
cjiles <57699863+cjiles@users.noreply.github.com>
2020-01-21 00:02:33 -0600

    Issues with Redux Thunk

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button,
     Modal, ModalHeader, ModalBody, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form'
+import { Loading } from './LoadingComponent';
 
 function RenderCampsite({campsite}){
     return(
@@ -140,6 +141,29 @@ class CommentForm extends Component{
 }
 
 function CampsiteInfo(props) {
+    
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
     if(props.campsite){
         return(
             <div className="container">
